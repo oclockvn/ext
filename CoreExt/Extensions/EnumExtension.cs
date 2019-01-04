d1a39380b57a018fc72d92d911ff1574693900fa
Quang Phan <oclockvn@gmail.com>
2019-01-04 10:50:05 +0700

    added enum extension

---
+++ CoreExt/Extensions/EnumExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace CoreExt.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get name of display attribute of an enum value
+        /// </summary>
+        /// <param name="enumValue">The enum value to get</param>
+        /// <returns>The name property of display attribute of exist, otherwise return enum to string</returns>
+        public static string ToDisplayName(this Enum enumValue)
+        {
+            var attr = enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .FirstOrDefault()
+                ?.GetCustomAttribute<DisplayAttribute>();
+
+            return attr != null ? attr.GetName() : enumValue.ToString();
+        }
+    }
+}
