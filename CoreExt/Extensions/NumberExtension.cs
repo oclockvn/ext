ace782c6b182ebaa67fcdc66c1605b6b83c8ee28
Quang Phan <oclockvn@gmail.com>
2019-01-04 11:38:55 +0700

    added number extension

---
+++ CoreExt/Extensions/NumberExtension.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+namespace CoreExt.Extensions
+{
+    public static class NumberExtensions
+    {
+        /// <summary>
+        /// Determine a string is number or not
+        /// </summary>
+        /// <param name="s">The string to check</param>
+        /// <returns>True if is a number. Otherwise return false</returns>
+        public static bool IsNumber(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return false;
+
+            return s.Length > 0 && s.All(char.IsDigit);
+        }
+
+        /// <summary>
+        /// try parse string to number, if fail return default value
+        /// </summary>
+        /// <param name="s">The s.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public static int ToNumber(this string s, int defaultValue)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return defaultValue;
+
+            return int.TryParse(s, out var number) ? number : defaultValue;
+        }
+
+        /// <summary>
+        /// try parse string to number, if fail return null
+        /// </summary>
+        /// <param name="s">The s.</param>
+        /// <returns></returns>
+        public static int? ToNumber(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return null;
+
+            if (int.TryParse(s, out var number))
+                return number;
+
+            return null;
+        }
+    }
+}
