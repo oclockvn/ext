cca62d36e25cd5a0365be334341e796895dfdca4
Quang Phan <oclockvn@gmail.com>
2019-01-04 10:30:21 +0700

    added hash helper

---
+++ CoreExt/Helpers/HashHelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+// ReSharper disable InconsistentNaming
+// ReSharper disable AccessToStaticMemberViaDerivedType
+
+namespace CoreExt.Helpers
+{
+    public static class HashHelper
+    {
+        /// <summary>
+        /// Supported hash algorithms
+        /// </summary>
+        public enum EHashType
+        {
+            HMAC, HMACMD5, HMACSHA1, HMACSHA256, HMACSHA384, HMACSHA512,
+            MACTripleDES, MD5, RIPEMD160, SHA1, SHA256, SHA384, SHA512
+        }
+
+        private static byte[] GetHash(string input, EHashType hash)
+        {
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+
+            switch (hash)
+            {
+                case EHashType.HMAC:
+                    return HMAC.Create().ComputeHash(inputBytes);
+
+                case EHashType.HMACMD5:
+                    return HMACMD5.Create().ComputeHash(inputBytes);
+
+                case EHashType.HMACSHA1:
+                    return HMACSHA1.Create().ComputeHash(inputBytes);
+
+                case EHashType.HMACSHA256:
+                    return HMACSHA256.Create().ComputeHash(inputBytes);
+
+                case EHashType.HMACSHA384:
+                    return HMACSHA384.Create().ComputeHash(inputBytes);
+
+                case EHashType.HMACSHA512:
+                    return HMACSHA512.Create().ComputeHash(inputBytes);
+
+                //case EHashType.MACTripleDES:
+                //    return MACTripleDES.Create().ComputeHash(inputBytes);
+
+                case EHashType.MD5:
+                    return MD5.Create().ComputeHash(inputBytes);
+
+                //case EHashType.RIPEMD160:
+                //    return RIPEMD160.Create().ComputeHash(inputBytes);
+
+                case EHashType.SHA1:
+                    return SHA1.Create().ComputeHash(inputBytes);
+
+                case EHashType.SHA256:
+                    return SHA256.Create().ComputeHash(inputBytes);
+
+                case EHashType.SHA384:
+                    return SHA384.Create().ComputeHash(inputBytes);
+
+                case EHashType.SHA512:
+                    return SHA512.Create().ComputeHash(inputBytes);
+
+                default:
+                    return inputBytes;
+            }
+        }
+
+        /// <summary>
+        /// Computes the hash of the string using a specified hash algorithm
+        /// </summary>
+        /// <param name="input">The string to hash</param>
+        /// <param name="hashType">The hash algorithm to use</param>
+        /// <returns>The resulting hash or an empty string on error</returns>
+        public static string ComputeHash(this string input, EHashType hashType = EHashType.SHA256)
+        {
+            var hash = GetHash(input, hashType);
+            var ret = new StringBuilder();
+
+            foreach (var t in hash)
+                ret.Append(t.ToString("x2"));
+
+            return ret.ToString();
+        }
+    }
+}
